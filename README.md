b5491544fa9ecbd025036ab139aac2adfb2ece0d
RadasPesa <pesaradim@gmail.com>
2024-11-07 08:47:56 +0100

    Inital setup for 3D project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity3D7GVIM2024
+Unity tutorial for 3D game development for 7GVIM course
---
+++ Unity3D7GVIM2024/Assets/Scripts/LightingManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class LightingManager : MonoBehaviour
+{
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightingPreset preset;
+    [SerializeField, Range(0, 24)] private float timeOfDay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeOfDay += Time.deltaTime;
+        timeOfDay %= 24;
+        UpdateLighting(timeOfDay / 24f);
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = preset.ambientColor.Evaluate(timePercent);
+        directionalLight.color = preset.directionalColor.Evaluate(timePercent);
+        directionalLight.transform.localRotation = Quaternion.Euler(new Vector3(
+            timePercent * 360f - 90f, 170f, 0));
+
+    }
+}
